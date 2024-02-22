c6a83a4215f568fc986ae9b98816c3218cf96fe8
João <joaombmrodrigues@gmail.com>
2024-02-22 22:09:47 +0000

    Add various char variables with unicode values and print them.

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -5,11 +5,20 @@ namespace VariousTypes
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //ex3.2
             int int_num = 10;
             short short_num = 47;
             long long_num = 128;
+            //ex3.3
             Console.WriteLine(int_num + " " + short_num + " " + long_num);
+            //ex3.5
+            char estimated_sign = '\u212E';
+            char heart = '\u2764';
+            char spades = '\u2660';
+            //ex3.6
+            Console.WriteLine(estimated_sign + " " + heart + " " + spades);
+            
         }
     }
 }
