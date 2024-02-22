95667283c541860d716b20571fd610b7d5208bfb
João <joaombmrodrigues@gmail.com>
2024-02-22 23:37:24 +0000

    Add new console project named 'AskForInput' that asks the user for an int number.

---
+++ AskForInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ex7.2.1
+            Console.WriteLine("Insert an integer number: ");
+            string response = Console.ReadLine();
+            
+        }
+    }
+}
