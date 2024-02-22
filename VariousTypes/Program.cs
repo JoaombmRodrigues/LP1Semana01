85442a74a1882cb99e15e2df8a306281a36ce2f1
João <joaombmrodrigues@gmail.com>
2024-02-22 21:28:56 +0000

    Add new console project named 'VariousTypes' that prints int variables.

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int_num = 10;
+            short short_num = 47;
+            long long_num = 128;
+            Console.WriteLine(int_num + " " + short_num + " " + long_num);
+        }
+    }
+}
