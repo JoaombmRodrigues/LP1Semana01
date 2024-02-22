7b134cb72454a928e12a1fd6ac29b38f8aea4531
João <joaombmrodrigues@gmail.com>
2024-02-22 22:50:14 +0000

    Add new console project named 'ManyStrings' that prints some strings.

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message1 = "Hello World!!!";
+            string message2 = "\"\u03A9\" really is \\u03A9";
+            string message3 = "Here comes the \u263C";
+            Console.WriteLine(message1);
+            Console.WriteLine(message2);
+            Console.WriteLine(message3);
+        }
+    }
+}
